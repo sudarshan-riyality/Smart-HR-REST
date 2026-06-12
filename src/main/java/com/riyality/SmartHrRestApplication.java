113c2d9cea221eb87aeabac64282a6b4802ea79d
sudarshan-riyality <sudarshan.riyality@gmail.com>
2026-06-12 11:42:12 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/riyality/SmartHrRestApplication.java
@@ -0,0 +1,13 @@
+package com.riyality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHrRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHrRestApplication.class, args);
+		System.out.println("Smart HR Backend Is In Running.......");
+	}
+}
---
+++ src/test/java/com/riyality/SmartHrRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riyality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHrRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
